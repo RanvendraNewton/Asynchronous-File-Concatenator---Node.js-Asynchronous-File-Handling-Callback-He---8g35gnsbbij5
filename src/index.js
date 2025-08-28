77eddc437222b0d469c5b2d4dc45f2322ca7ef09
RanvendraNewton <ranvendra.singh2024@nst.rishihood.edu.in>
2025-08-28 12:50:59 +0530

    DONE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
+const fs = require('fs');
+
 async function cat(filePaths, outputFilePath) {
   // write your code here
+  const results = [];
+
+  for (const path of filePaths) {
+
+    try {
+
+      const stat = await fs.stat(path);
+
+      if (stat.isFile()) {
+        const content = await fs.readFile(path, 'utf-8');
+        results.push(content)
+      }
+      else if (stat.isDirectory()) {
+        results.push('Is a directory')
+      }
+      else {
+        results.push('File not found')
+      }
+
+    }
+    catch(error) {
+
+      results.push('File not found')
+
+    }
+  }
+
+  await fs.writeFile(outputFilePath, results.join('\n'), 'utf-8')
+
+
+
 }
 
 module.exports = { cat };
